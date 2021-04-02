19cae83ed5b6c4a886437a5fd5c6eb1ee26855e2
Error.EXE/?¿| <81505646+dev666EXE@users.noreply.github.com>
2021-04-02 15:04:02 +0100

    Update PerX.py

--- PerX.py
+++ PerX.py
@@ -1,32 +1,40 @@
 print ("Bem-vindo ao PerX")
 print()
+print ("»Criador-Stefan Monteiro")
+print ("»Whatssap-935461678«")
+print ("»[Descenders]«"]
+print()
 print ("[Soma] |1|")
-print ("[Subtra��o] |2|")
-print ("[Divis�o] |3|")
-print ("[Multiplica��o] |4|")
-boga = int(input("Qual opera��o voce deseja calcular ?\n"))
+print()
+print ("[Subtração] |2|")
+print()
+print ("[Divisão] |3|")
+print()
+print ("[Multiplicação] |4|")
+print()
+boga = int(input("Qual operação voce deseja calcular ?\n"))
 
 
 if (boga == 1):
     s = int(input ("Digite o primeiro numero:"))
     t = int(input ("Segundo o primeiro numero:"))
     torra = s + t
-    print ("O resultado �:" ,torra)
+    print ("O resultado é:" ,torra)
     print ()
 if (boga == 2):
     y = int(input("Digite o primeiro numero:"))
     p = int(input("Segundo o primeiro numero:"))
     bota = y - p
-    print("O resultado �:", bota)
+    print("O resultado é:", bota)
 
 if (boga == 3):
     y = int(input("Digite o primeiro numero:"))
     p = int(input("Segundo o primeiro numero:"))
     bota = y / p
-    print("O resultado �:", bota)
+    print("O resultado é:", bota)
 
 if (boga == 4):
     y = int(input("Digite o primeiro numero:"))
     p = int(input("Segundo o primeiro numero:"))
     bota = y * p
-    print("O resultado �:", bota)
+    print("O resultado é:", bota)
